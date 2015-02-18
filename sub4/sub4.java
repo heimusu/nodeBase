243a530c06728571635744a94c8ffaba4cd39d38
heimusu <heimusu@gmail.com>
2015-02-18 16:57:26 +0900

    演習4の途中まで

--- sub4/sub4.java
+++ sub4/sub4.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import org.w3c.dom.*;
+import javax.xml.xpath.*;
 import javax.xml.parsers.*;
 import java.util.*;
 
@@ -9,7 +10,6 @@ public class sub4{
         NodeList nodeX;
         Node att = null;
         int i,j;
-        int depth = 0;
         String sv;
 
         Document document = DocumentBuilderFactory
@@ -19,7 +19,13 @@ public class sub4{
         node = (Node)(document.getDocumentElement());
         FileOutputStream xyz = new FileOutputStream("output.xml");
         OutputStreamWriter out = new OutputStreamWriter(xyz, "utf-8");
-        //System.out.println("\nルートノード名:" + node.getNodeName());
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+
+
+
+        System.out.println("\nルートノード名:" + node.getNodeName());
         String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
         out.write(header);
         //属性チェック
@@ -30,10 +36,9 @@ public class sub4{
         else{
             out.write("<" + node.getNodeName() + ">\n" );
         }
-        //System.out.println("ルートノードタイプ:" + node.getNodeType());
-        //System.out.println("その子要素数:" + node.getChildNodes().getLength());
+        System.out.println("ルートノードタイプ:" + node.getNodeType());
+        System.out.println("その子要素数:" + node.getChildNodes().getLength());
         ndTemp = node.getFirstChild();
-        depth++;
         for(i = 1; ndTemp != null; i++, ndTemp = ndTemp.getNextSibling()){
             //改行チェック
             if(ndTemp.getNodeType() == 3 && check(ndTemp) == false){
@@ -41,7 +46,7 @@ public class sub4{
             }
             //改行でない場合
             else{
-                //System.out.println("ノードの名前:" + ndTemp.getNodeName() + "ノードタイプ:" + ndTemp.getNodeType());
+                System.out.println("ノードの名前:" + ndTemp.getNodeName() + "ノードタイプ:" + ndTemp.getNodeType());
                 att = attCheck(ndTemp,att);
                 if(att != null){
                     out.write("\t <" + ndTemp.getNodeName() + " " + att + ">");
@@ -51,26 +56,26 @@ public class sub4{
                 }
                 //文字列が含まれているか？
                 Node ndTemp2 = ndTemp.getFirstChild();
-                if(ndTemp.getNodeType() == 1 && ndTemp2 != null){
+                if(ndTemp.getNodeType() == 1 && ndTemp2 != null && ndTemp2.getNodeType() == 3){
                     String output = ndTemp2.getNodeValue();
+                    System.out.println(ndTemp2.getNodeType());
                     output = output.replaceAll("\n","");
-                    output = output.replaceAll(" ","");
+                    //output = output.replaceAll(" ","");
                     if(output.length() != 0){
-                        //System.out.println("エレメント内データ:\"" + output + "\"");
+                        System.out.println("エレメント内データ:\"" + output + "\"");
                         out.write(output + "</" + ndTemp.getNodeName() + ">\n");
                     }
                 }
                 //次の要素を見つけに行く
                 ndTemp2 = ndTemp2.getNextSibling();
                 if(ndTemp2 != null){
-                    depth++;
                     out.write("\n");
                     for(j = 1; ndTemp2 != null; j++,ndTemp2 = ndTemp2.getNextSibling()){
                         if(ndTemp2.getNodeType() == 3 && check(ndTemp2) == false){
                             continue;
                         }
                         else{
-                            //System.out.println("ノードの名前:" + ndTemp2.getNodeName() + "ノードタイプ:" + ndTemp2.getNodeType());
+                            System.out.println("ノードの名前:" + ndTemp2.getNodeName() + "ノードタイプ:" + ndTemp2.getNodeType());
                             att = attCheck(ndTemp2,att);
                             if(att != null){
                                 out.write("\t\t<" + ndTemp2.getNodeName() + " " + att + ">");
@@ -80,20 +85,83 @@ public class sub4{
                             }
                             //文字列が含まれているか？
                             Node ndTemp3 = ndTemp2.getFirstChild();
-                            if(ndTemp2.getNodeType() == 1 && ndTemp3 != null){
+                            if(ndTemp2.getNodeType() == 1 && ndTemp3 != null && ndTemp3.getNodeType() == 3){
                                 String output = ndTemp3.getNodeValue();
                                 output = output.replaceAll("\n","");
-                                output = output.replaceAll(" ","");
+                                //output = output.replaceAll(" ","");
                                 if(output.length() != 0){
-                                    //System.out.println("エレメント内データ:\"" + output + "\"");
+                                    System.out.println("エレメント内データ:\"" + output + "\"");
                                     out.write(output + "</" + ndTemp2.getNodeName() + ">\n");
                                 }
+                            }
+                            //4段目までは見つけられるように
+                            ndTemp3 = ndTemp3.getNextSibling();
+                            if(ndTemp3 != null){
+                                out.write("\n");
+                                for(j = 1; ndTemp3 != null; j++,ndTemp3 = ndTemp3.getNextSibling()){
+                                    if(ndTemp3.getNodeType() == 3 && check(ndTemp3) == false){
+                                        continue;
+                                    }
+                                    else{
+                                        System.out.println("ノードの名前:" + ndTemp3.getNodeName() + "ノードタイプ:" + ndTemp3.getNodeType());
+                                        att = attCheck(ndTemp3,att);
+                                        if(att != null){
+                                            out.write("\t\t\t<" + ndTemp3.getNodeName() + " " + att + ">");
+                                        }
+                                        else{
+                                            out.write("\t\t\t<" + ndTemp3.getNodeName() + ">");
+                                        }
+                                        //文字列が含まれているか？
+                                        Node ndTemp4 = ndTemp3.getFirstChild();
+                                        if(ndTemp3.getNodeType() == 1 && ndTemp4 != null && ndTemp4.getNodeType() == 3){
+                                            String output = ndTemp4.getNodeValue();
+                                            output = output.replaceAll("\n","");
+                                            //output = output.replaceAll(" ","");
+                                            if(output.length() != 0){
+                                                System.out.println("エレメント内データ:\"" + output + "\"");
+                                                out.write(output + "</" + ndTemp3.getNodeName() + ">\n");
+                                            }
+                                        }
+                                        //5段目までは見つけられるように
+                                        ndTemp4 = ndTemp4.getNextSibling();
+                                        if(ndTemp4 != null){
+                                            out.write("\n");
+                                            for(j = 1; ndTemp4 != null; j++,ndTemp4 = ndTemp4.getNextSibling()){
+                                                if(ndTemp4.getNodeType() == 3 && check(ndTemp4) == false){
+                                                    continue;
+                                                }
+                                                else{
+                                                    System.out.println("ノードの名前:" + ndTemp4.getNodeName() + "ノードタイプ:" + ndTemp4.getNodeType());
+                                                    att = attCheck(ndTemp4,att);
+                                                    if(att != null){
+                                                        out.write("\t\t\t\t<" + ndTemp4.getNodeName() + " " + att + ">");
+                                                    }
+                                                    else{
+                                                        out.write("\t\t\t\t<" + ndTemp4.getNodeName() + ">");
+                                                    }
+                                                    //文字列が含まれているか？
+                                                    Node ndTemp5 = ndTemp4.getFirstChild();
+                                                    if(ndTemp4.getNodeType() == 1 && ndTemp5 != null && ndTemp5.getNodeType() == 3){
+                                                        String output = ndTemp5.getNodeValue();
+                                                        output = output.replaceAll("\n","");
+                                                        //output = output.replaceAll(" ","");
+                                                        if(output.length() != 0){
+                                                            System.out.println("エレメント内データ:\"" + output + "\"");
+                                                            out.write(output + "</" + ndTemp4.getNodeName() + ">\n");
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            out.write("\t\t\t</" + ndTemp3.getNodeName() + ">\n");
+                                        }
+                                    }
+                                }
+                                out.write("\t\t</" + ndTemp2.getNodeName() + ">\n");
                             }
                         }
                     }
                     out.write("\t</" + ndTemp.getNodeName() + ">\n");
                 }
-                depth--;
             }
         }
         out.write("</" + node.getNodeName() + ">");
